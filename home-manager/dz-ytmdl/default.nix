ad79dddf9ed462bce782515d3d4633cd6d1fe460
Mitch Dzugan <mitchdzugan@gmail.com>
2026-04-28 14:12:39 -0500

    create all lock files

--- home-manager/dz-ytmdl/default.nix
+++ home-manager/dz-ytmdl/default.nix
@@ -2,5 +2,5 @@
   pname = "dz-ytmdl";
   version = "1.0.0";
   src = ./.;
-  npmDepsHash = "sha256-PmmaYZ3VNQiOYfaH9aHYRmbRNdU9S3CsKdFR+b9k0aA=";
+  npmDepsHash = "sha256-wzRp7i0Bj5SOk3MiwGDAowDtEAqI3Nd7pHLj98J1GB0=";
 }
